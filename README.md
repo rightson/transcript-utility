2295086eb72ce18d71aa42a357c9eebdb2f9d7b0
Scott Chen <rightson@gmail.com>
2023-05-27 23:30:09 +0800

    Provide ytt.clean and update usage

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Youtube Transcript Generator
+# YouTube Transcript Generator
 
 Usage:
 ```py
@@ -7,6 +7,16 @@ from yt_transcript import YouTubeTranscript
 url = 'https://www.youtube.com/watch?v=8p0oCUE3mWE'
 output_name = 'jenson-2023-ntu-commencement'
 
-YouTubeTranscript(url, output_name).get_transcript()
+ytt = YouTubeTranscript(url, output_name)
+ytt.get_transcript()
+```
 
+Using the `ytt` object, the video from the provided URL is downloaded with the name specified in `output_name`, and the transcript is saved as `{output_name}.txt`.
+
+The `ytt` object supports incremental transcription. If `ytt.get_transcript()` gets interrupted unexpectedly, you can re-run the command and the transcription will resume where it left off.
+
+Upon successful completion of the transcription, you can call `ytt.clean()` to remove all intermediate files. Be aware, however, that once `ytt.clean()` is called, all the intermediate files will be removed and the incremental transcription process will be reset.
+
+```py
+ytt.clean()
 ```
--- yt_transcript.py
+++ yt_transcript.py
@@ -1,20 +1,19 @@
 import os
+import shutil
 import openai
 from pytube import YouTube
 from pydub import AudioSegment
 
 
 class YouTubeTranscript:
-    def __init__(self, url, output_name, output_transcript='', open_api_key=''):
+    def __init__(self, url, output_name, open_api_key=''):
         self.url = url
-        if not output_transcript:
-            output_transcript = output_name
         self.output_name = output_name
-        self.output_transcript = output_transcript
-        if not os.path.exists(output_transcript):
-            os.makedirs(output_transcript)
-            os.makedirs(os.path.join(output_transcript, 'chunks'))
-        self.audio = f'{output_transcript}/{output_name}'
+        self.output_transcript = f'{output_name}.txt'
+        if not os.path.exists(output_name):
+            os.makedirs(output_name)
+            os.makedirs(os.path.join(output_name, 'chunks'))
+        self.audio = f'{output_name}/{output_name}'
         self.audio_file_name = f'{self.audio}.wav'
         openai.api_key = os.environ.get('OPENAI_API_KEY') or open_api_key
 
@@ -23,7 +22,7 @@ class YouTubeTranscript:
         transcript = ''
         chunks = self.get_chunks_by_seconds(input_audio, chunk_second)
         for i, chunk in enumerate(chunks):
-            chunk_transcript_path = f'{self.output_transcript}/chunks/{self.output_name}_chunk{i}.txt'
+            chunk_transcript_path = f'{self.output_name}/chunks/{self.output_name}_chunk{i}.txt'
             exists = os.path.exists(chunk_transcript_path)
             print(f'Finding {chunk_transcript_path}', exists)
             if not exists:
@@ -35,9 +34,9 @@ class YouTubeTranscript:
                 chunk_transcript = self.read_transcript(chunk_transcript_path)
             transcript += chunk_transcript
         if transcript:
-            with open(f'{self.audio}.txt', 'w') as f:
+            with open(self.output_transcript, 'w') as f:
                 f.write(transcript)
-            print(f'Saved transcript to {self.audio}.txt')
+            print(f'Saved transcript to {self.output_transcript}')
         return transcript
 
     def get_input_audio(self):
@@ -45,7 +44,7 @@ class YouTubeTranscript:
             yt = YouTube(self.url)
             audio_stream = yt.streams.filter(only_audio=True).first()
             print(f'Downloading {self.audio} from {self.url}')
-            audio_stream.download(output_path=self.output_transcript, filename=self.output_name)
+            audio_stream.download(output_path=self.output_name, filename=self.output_name)
         if not os.path.exists(self.audio_file_name):
             print(f'Exporting {self.audio_file_name} from {self.audio}')
             input_audio = AudioSegment.from_file(self.audio)
@@ -61,7 +60,7 @@ class YouTubeTranscript:
         return chunks
 
     def get_chunk_file(self, i, chunk):
-        prefix_name = f'{self.output_transcript}/chunks/{self.output_name}_chunk{i}'
+        prefix_name = f'{self.output_name}/chunks/{self.output_name}_chunk{i}'
         full_name = f'{prefix_name}.wav'
         if not os.path.exists(full_name):
             print(f'Exporting new chunk file {full_name}')
@@ -71,11 +70,11 @@ class YouTubeTranscript:
             chunk_file = open(full_name, 'rb')
         return chunk_file
 
-    def transcribe(self, input_audio, output_transcript):
+    def transcribe(self, input_audio, output_file):
         with input_audio as f:
             result = openai.Audio.transcribe('whisper-1', f)
             text = result['text']
-            with open(output_transcript, 'w') as f:
+            with open(output_file, 'w') as f:
                 f.write(text)
             print('Transcribed:', text)
             return text
@@ -85,3 +84,6 @@ class YouTubeTranscript:
             text = f.read()
             print('Transcript:', text)
             return text
+
+    def clean(self):
+        shutil.rmtree(self.output_name)
